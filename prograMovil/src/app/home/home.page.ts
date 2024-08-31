767bf7810b436c84bfad2d1640ee6c87d893a7d9
Belén <bele.diaz@duocuc.cl>
2024-08-31 17:04:00 -0400

    HTML Login
    HTML del login con su css

---
+++ prograMovil/src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  /*obj para el usuario */
+  user = {
+    username:'',
+    password:'',
+  };
+
+  mensaje = '';
+  spinner=false;
+
+  constructor(private router: Router, private animationController: AnimationController) {
+  }
+
+validarLogin(){
+  if (this.user.username.length != 0){
+    if (this.user.password.length != 0){
+      this.mensaje = 'Acceso concedido';
+      let navigationExtras: NavigationExtras = {
+        state: {
+          username: this.user.username,
+          password: this.user.password,
+        },
+      };
+      
+      } else {
+        console.log('Contraseña invalida');
+        this.mensaje = 'Contraseña invalida';
+        //No funciona
+      }
+    } else {
+      console.log('Usuario invalido');
+      this.mensaje = 'Usuario invalido';
+    
+    }
+  }
+}
+
