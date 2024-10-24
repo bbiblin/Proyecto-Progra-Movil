558e8e5e125036616b4fd774bf9780c2a4dd2ff2
Lucero00 <134719275+Luceritox@users.noreply.github.com>
2024-10-24 18:16:52 -0300

    Página de error
    Se creo la página de error que redirige a home cuando se presiona el vinculo (aquí)

--- prograMovil/src/app/app-routing.module.ts
+++ prograMovil/src/app/app-routing.module.ts
@@ -25,6 +25,10 @@ const routes: Routes = [
     path: 'registro',
     loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
   },
+  {
+    path: 'error',
+    loadChildren: () => import('./error/error.module').then( m => m.ErrorPageModule)
+  },
 
 
 ];
